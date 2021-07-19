536b540203ba0ceea1aeb85d1f56acd0abcd3775
Michael Zidar <mzidar@gmail.com>
2021-07-19 17:21:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1, num2, multiply, random, mod, num3, num4, max;
+num1 = 2;
+num2 = 31;
 
-// Write your code here
+multiply = num1*num2; 
+
+random = Math.ceil(Math.random() * 10) 
+
+num3 = 4;
+num4 = 10;
+
+mod = 10*num3/num4;
+
+max = (0, 4, 20);
+
+Math.max(max);
